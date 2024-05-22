aeae5b171231ab58f79cffcba45a77f3af3cea84
k3y10 <k3yt3n@gmail.com>
2024-05-21 18:14:44 -0600

    simple updates

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,16 @@ export default function Page() {
             layout="fixed"
           />
         </div>
-        <h1 className="text-2xl font-semibold tracking-tighter">
-          k3y10 - about me
-        </h1>
       </div>
       <p className="mb-4">
+      <h1 className="text-2xl font-semibold tracking-tighter mb-4">About me</h1>
+
         {`I'm a dynamic technical leader with 8+ years of experience, previously at Tesla, transitioning from self-taught hacking and full-stack web development to spearheading innovations in cryptography, blockchain technology, quantum laser communication, and embedded AI software. My journey began in cybersecurity and software development, where I bootstrapped platforms and security teams for startups across various funding stages. Today, I lead groundbreaking efforts at the forefront of technology, developing new theoretical working models for quantum laser communication solutions and advancing embedded AI software-related products to redefine industry standards across various platforms.`}
       </p>
+
+      
       <div className="my-8">
+      <h1 className="text-2xl font-semibold tracking-tighter mb-4">Portfolio</h1>
         <BlogPosts />
       </div>
     </section>
