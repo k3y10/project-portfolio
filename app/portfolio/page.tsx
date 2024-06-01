8a64f256a2ecf3e276e059b53cde5e308b2d22ef
k3y10 <k3yt3n@gmail.com>
2024-06-01 13:33:48 -0600

    updated iframe design rendering issue and project posts

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section className="flex flex-col items-center justify-center p-4">
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter text-center">My Tech Portfolio</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter text-center">Project Timelines</h1>
       <div className="w-full flex justify-center">
         <BlogPosts />
       </div>
